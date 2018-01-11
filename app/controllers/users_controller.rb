11331ab19d832b6b3a811194aff768174f21d96e
Rob Cho <robhcho@gmail.com>
2018-01-10 21:36:37 -0500

    Make a create action in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      full_name: params[:full_name],
+      username: params[:username],
+      password: params[:password],
+      confirmation_password: params[:confirmation_password]
+    )
+    if user.save
+      render json: user.as_json
+    else
+      render json: { error: user.errors.full_messages }
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :votes
   has_many :polls, through: :votes
+
+  validates :full_name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  # validates :password, length: { in: 6..20 }
 end
