9c6de378689b668b275e06dae70596cbfd2eabc1
Rob Cho <robhcho@gmail.com>
2018-01-17 22:13:56 -0500

    Begin adding create action to Events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+  def create
+    event = Event.new(
+      user_id: current_user.id,
+      event_title: params[:event_title],
+      date_time: params[:date_time],
+      event_duration: params[:event_duration]
+      )
+    if event.save
+      render json: event.as_json
+    else
+      render json: { errors: event.errors.full_messages }
+    end
+  end
 end
